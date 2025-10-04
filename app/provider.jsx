25c646ec35ffb7e720d472d10ac7313b1570d293
kbanyal10 <kbanyal10@gmail.com>
2025-10-04 19:42:03 -0400

    Theme Change and Side Bar added

--- app/provider.jsx
+++ app/provider.jsx
@@ -10,7 +10,7 @@ function Provider({ children, ...props }) {
       disableTransitionOnChange
       {...props}
     >
-      <div>Provider</div>
+      {children}
     </NextThemesProvider>
   );
 }
